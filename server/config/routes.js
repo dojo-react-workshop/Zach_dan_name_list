b3d37f2c632f3576461614b26cd337fccc2bbbad
dandrade1206 <dandrade1206@gmail.com>
2017-05-26 15:17:19 -0500

    added bootstrap

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,7 @@
 module.exports = function (app) {
 
     app.get('/', function (req, res){
-        res.send('hi');
-        // res.render('index');
+        //res.send('hi');
+        res.render('index');
     })
 }
