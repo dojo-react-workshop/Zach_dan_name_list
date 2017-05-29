2d6b8145021b35b0005a232227b5c0c32bf7e70d
dandrade1206 <dandrade1206@gmail.com>
2017-05-29 08:20:43 -0500

    changes for add item

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,18 @@
 module.exports = function (app) {
 
-    app.get('/', function (req, res){
-        //res.send('hi');
-        res.render('index');
+    var nameList = [
+        ]
+
+    app.get('/', function (req, res){ 
+       res.render("index")
+    })
+    
+    app.get('/list', function (req, res){ 
+       res.json(nameList)
+    })
+
+     app.post('/addName',(req,res)=>{
+		nameList.push(req.body);
+		res.json(req.body);
     })
 }
